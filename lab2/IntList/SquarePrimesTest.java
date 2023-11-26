2cca60f66f7154a12475f59af959594a41cee781
Calsight <catcallight@gmail.com>
2023-11-26 16:15:58 +0800

    first commit lab1 and lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -13,8 +13,24 @@ public class SquarePrimesTest {
     @Test
     public void testSquarePrimesSimple() {
         IntList lst = IntList.of(14, 15, 16, 17, 18);
-        boolean changed = IntListExercises.squarePrimes(lst);
+        boolean changed = IntListExercises.squarePrimes(lst,false);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(8, 9, 10);
+        boolean changed = IntListExercises.squarePrimes(lst,false);
+        assertEquals("8 -> 9 -> 10", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimes1() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5, 6);
+        boolean changed = IntListExercises.squarePrimes(lst,false);
+        assertEquals("1 -> 4 -> 9 -> 4 -> 25 -> 6", lst.toString());
         assertTrue(changed);
     }
 }
