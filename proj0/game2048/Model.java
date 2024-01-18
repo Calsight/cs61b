4d934711f9fca7aa89c23fc61c5d8d9276a18a1c
Calsight <catcallight@gmail.com>
2024-01-18 12:14:43 +0800

    commit

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -110,9 +110,63 @@ public class Model extends Observable {
         boolean changed;
         changed = false;
 
-        // TODO: Modify this.board (and perhaps this.score) to account
+        // Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
+        board.setViewingPerspective(side);
+//        Tile t = board.tile(2, 1);
+//        board.move(2, 3, t);
+//        t = board.tile(2, 0);
+//        board.move(2, 2, t);
+
+//        "We anticipate that tilt will take you 3 to 10 hours to complete. " from TA
+//        finish it in 2021/03/03 17:40 - 19:02
+//        1.5h ^^
+
+//      board.move(c, r, t) 是已经计算好要移动到这个位置的了
+        /**
+         *  c,r
+         *  0,3  1,3  2,3  3,3
+         *  0,2  1,2  2,2  3,2
+         *  0,1  1,1  2,1  3,1
+         *  0,0  1,0  2,0  3,0
+         */
+        boolean[][] merged = new boolean[board.size()][board.size()];
+        for (int r = 2; r >= 0; r--){
+            for (int c = 0; c <= 3; c++){
+                Tile t = board.tile(c,r);
+
+                if (t != null) {
+                    int dc = c;
+                    int dr = r + 1;
+
+                    while (dr <= 3) {
+                        Tile dt = board.tile(dc,dr);
+                        if (dt != null) {
+                            if (merged[dc][dr] == true || dt.value() != t.value()) {
+                                dr--;
+                            }
+                            break;
+                        }
+                        if (dr == 3){
+                            break;
+                        }
+                        dr++;
+                    }
+
+                    if (dr != r){
+                        changed = true;
+                    }
+                    if (board.move(dc, dr, t)){
+                        merged[dc][dr] = true;
+                        score += board.tile(dc,dr).value();
+                    }
+                }
+            }
+        }
+
+
+        board.setViewingPerspective(Side.NORTH);
 
         checkGameOver();
         if (changed) {
@@ -120,6 +174,7 @@ public class Model extends Observable {
         }
         return changed;
     }
+
 
     /** Checks if the game is over and sets the gameOver variable
      *  appropriately.
@@ -138,6 +193,11 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (Tile t : b) {
+            if (t == null) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -148,7 +208,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (Tile t : b) {
+            if (t != null && t.value() == MAX_PIECE) {
+                return true;
+            }
+        }
         return false;
+
     }
 
     /**
@@ -159,6 +225,23 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        return emptySpaceExists(b) || mergeMoveExists(b);
+    }
+    private static boolean mergeMoveExists(Board b) {
+        int[][] neighbors = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 上下左右
+        int len = b.size();
+        for (Tile t : b) {
+            int c = t.col();    // x
+            int r = t.row();    // y
+            for (int i = 0; i < 4; i++) {
+                int nc = c + neighbors[i][0];
+                int nr = r + neighbors[i][1];
+                if ((0 <= nc && nc < len) && (0 <= nr && nr < len)
+                        && (b.tile(nr, nc).value() == t.value())) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
